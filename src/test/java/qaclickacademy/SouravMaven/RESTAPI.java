72be52a5abd569274058a8f3d96bfc6e196c5b23
SouravMohapatra089 <msaurav089@gmail.com>
2020-10-23 12:36:04 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SouravMaven/RESTAPI.java
@@ -0,0 +1,15 @@
+package qaclickacademy.SouravMaven;
+
+import org.testng.annotations.Test;
+
+public class RESTAPI {
+	@Test
+	public  void PostJIRA() {
+		System.out.println("PostJIRA");
+	}
+	
+	@Test
+	public  void DeleteTwitter() {
+		System.out.println("Delete Twitter");
+	}
+}
